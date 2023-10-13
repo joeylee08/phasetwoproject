5833ae0a92e3406c590f78d0ce8a732cbbc85b5e
Brian Richie Sr <brianrichiesr@gmail.com>
2023-10-13 15:10:17 -0700

    add browser router and set nav links

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -1,7 +1,12 @@
-
+import NavBar from "./NavBar"
 
 const MainBody = () => {
-
+    return (
+        <>
+            <NavBar />
+            <h1>Hello World!</h1>
+        </>
+    )
 }
 
 export default MainBody
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import * as React from "react";
+import { Link } from "react-router-dom";
+import { v4 as uuid } from "uuid";
+
+const links = ["Link-1", "Link-2", "Link-3", "Link-4"];
 
 const NavBar = () => {
+    const mappedLinks = links.map(link => {
+        return <Link className="navLink" key={uuid()} to={link[0]}>{link}</Link>
+    })
 
+    return (
+    <header>
+        {mappedLinks}
+    </header>
+    )
 }
 
 export default NavBar
