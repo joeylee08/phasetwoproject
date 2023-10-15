1f0b8945fe296017022c8b1a80073da278918a46
Brian Richie Sr <brianrichiesr@gmail.com>
2023-10-15 10:14:18 -0700

    add minor adjustments

--- src/components/HiddenNavBar.js
+++ src/components/HiddenNavBar.js
@@ -1,11 +1,27 @@
 import * as React from "react";
+import { NavLink } from "react-router-dom";
+import { v4 as uuid } from "uuid";
+
+const links = ["Home"];
 
 const HiddenNavBar = () => {
+    
+    const mappedLinks = links.map(link => {
+        return (    
+            <NavLink
+                to={`/`}
+                key={uuid()}
+                className={"navLink"}
+            >
+                {link}
+            </NavLink>
+        )
+    })
 
     return (
         <header>
-            <nav className="hidden">
-                Login or Sign Up
+            <nav>
+                {mappedLinks}
             </nav>
         </header>
     )
