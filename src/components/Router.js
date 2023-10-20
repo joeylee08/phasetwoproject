6bb6b166ca399cec013947347ce4e60b39f32e90
Joseph <josephylee72@gmail.com>
2023-10-19 17:53:07 -0700

    removed store, minor tweaking

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,8 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import About from "./About";
 import Instructions from "./Instructions";
 import LeaderBoard from "./LeaderBoard";
-import SudokuStore from "./SudokuStore";
 import YourPuzzles from "./YourPuzzles";
 import MainBody from "./MainBody";
 import LoginModal from "./LoginModal";
@@ -12,11 +10,9 @@ import Error from "./Error";
 function Router({ showApp, onContinueAsGuest, onLoginSuccess, handleSetCurrentUser}) {
     const routes =  ( 
         <>
-            <Route path="/about" element={<About />} />,
             <Route path="/leader-board" element={<LeaderBoard />} />,
             <Route path="/instructions" element={<Instructions />} />,
             <Route path="/your-puzzles" element={<YourPuzzles />} />,
-            <Route path="/sudoku-store" element={<SudokuStore />} />
             <Route path="/loading" element={<Loading/>} />
             <Route path="/loading/:param" element={<Loading/>} />
         </>
